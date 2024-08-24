72bfa89029de91826c87d9653e2be87f0d898fed
vv68 <vv68@bk.ru>
2024-08-24 19:44:50 +0300

    add api features

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -43,9 +43,11 @@ AppAsset::register($this);
         'options' => ['class' => 'navbar-nav me-auto mb-2 mb-md-0'],
         'items' => $menuItems,
     ]);
+
     if (Yii::$app->user->isGuest) {
         echo Html::tag('div',Html::a(Yii::t('app', 'Login'),['/site/login'],['class' => ['btn btn-link login text-decoration-none']]),['class' => ['d-flex']]);
     } else {
+        echo Html::tag('div',Html::a(Yii::t('app', 'API'),['/site/api-doc'],['class' => ['btn btn-link text-decoration-none']]),['class' => ['d-flex']]);
         echo Html::beginForm(['/site/logout'], 'post', ['class' => 'd-flex'])
             . Html::submitButton(
                 'Logout (' . Yii::$app->user->identity->username . ')',
